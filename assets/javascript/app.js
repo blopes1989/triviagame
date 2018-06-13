7c89da4961bc74dfb129a3a6fdc48276b9b50f45
bizzy <blopes1989@gmail.com>
2018-06-12 23:24:46 -0400

    questions scroll

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,62 +1,71 @@
 $(document).ready(function () {
 //variable
-var correct 
-var incorrect
+var correct = 0
+var incorrect = 0
 var start
 var retry
 var locale = 0
 //questions & awnsers
 
    var question0 = {
-    query: "Who is the founder of Marvel Comics ?",
+    trivia: "Who is the founder of Marvel Comics ?",
     choiceA: "Stan Lee",
     choiceB: "Martin Goodman",
     choiceC: "Bruce Wayne",
     choiceD: "Adam Warlock",
+    awnser: "choiceB",
+    
     }
 
     var question1 = {
-        query: "What is Superman's weakness ?",
+        trivia: "What is Superman's weakness ?",
         choiceA: "Women",
         choiceB: "Water",
         choiceC: "Tv",
         choiceD: "Kyrptonite",
+        awnser: "choiceD"
     }
     
     var question2 = {
-        query: "How many Robin's have existed in the Batman storyline ?",
+        trivia: "How many Robin's have existed in the Batman storyline ?",
         choiceA: "2",
         choiceB: "3",
         choiceC: "4",
         choiceD: "5",
+        awnser: "choiceD"
     }
     var question3 = {
-        query: "Who were the orignal X-Men ?",
+        trivia: "Who were the orignal X-Men ?",
         choiceA: "Cyclops, Marvel Girl, Beast, Angel, Iceman",
         choiceB: "Cyclops, Jean Gray, Wolverine, Storm, Proffesor Xavier",
        choiceC: "Gambit, Beast, Magneto, Iceman, Nightcrawler",
        choiceD: "Wolverine, Nightcrawler, Colossus, Warpath, Gambit", 
+        awnser: "choiceA"
     }
     var question4 = {
-        query: "Who is Deadpool based on ?",
+        trivia: "Who is Deadpool based on ?",
         choiceA: "Wolverine",
         choiceB: "Deathstrike",
         choiceC: "Hulk",
         choiceD: "Black Panther",
+        awnser: "choiceA"
     }
     var question5 = {
-        query: "What color was the original Hulk ?",
+        trivia: "What color was the original Hulk ?",
         choiceA: "Blue",
         choiceB: "Red",
         choiceC: "Gray",
         choiceD: "Green",
+        awnser: "choiceC"
+    
     }
     var question6 = {
-        query: "How many Iron Man suits has Tony Stark created ?",
+        trivia: "How many Iron Man suits has Tony Stark created ?",
         choiceA: "5",
         choiceB: "17",
         choiceC: "30",
         choiceD: "Over 50",
+        awnser: "choiceD"
     }
     var question7 = {
         query: "Who was a Batman love interest ?",
@@ -64,20 +73,23 @@ var locale = 0
         choiceB: "Batgirl",
         choiceC: "Wonder Woman",
         choiceD: "All of the above",
+        awnser: "choiceD"
     }
     var question8 = {
-        query: "What comic book character did Michael Jackson want to play in the movies ?",
+        trivia: "What comic book character did Michael Jackson want to play in the movies ?",
         choiceA: "Howard the Duck",
         choiceB: "Spiderman",
         choiceC: "Green Arrow",
         choiceD: "Dr. Strange",
+        awnser: "choiceB"
     }
     var question9 = {
-        query: "Who owns the rights to the word superhero ?",
+        trivia: "Who owns the rights to the word superhero ?",
         choiceA: "DC",
         choiceB: "Marvel",
         choiceC: "Both companies",
         choiceD: "Neither/someone else",
+        awnser: "choiceC"
     }
     var questionArray = [question0, question1, question2, question3, question4, question5, question6, question7, question8, question9] 
 //functions
@@ -87,20 +99,76 @@ var locale = 0
  // listeners
  i=0
 
- $("#trivquestion").text(questionArray[i].query);
+ function loading(){
+// starting here create a function that loads the next question
+ $("#trivQuestion").text(questionArray[i].trivia);
  $("#choiceA").text(questionArray[i].choiceA);
  $("#choiceB").text(questionArray[i].choiceB);
  $("#choiceC").text(questionArray[i].choiceC);
  $("#choiceD").text(questionArray[i].choiceD);
 // $("#display").text(questionArray)
+ };
 
+
+loading();
+
+//correct and incorrect
+$("#totalCorrect").text(correct);
+$("#totalWrong").text(incorrect)
 //place questions on screen
+
 $("#red").on("click", function() {
-     i++; })
+     i++; console.log("clicked red");
+    if ((questionArray[i].awnser) === $(choiceA)){
+        correct++;
+        $("#totalCorrect").text(correct);
+
+    }else{
+     incorrect++;
+     $("#totalWrong").text(incorrect)
+    }
+    loading();
+
+ })
+    //call function that loads the next question see line 
+    $("#blue").on("click", function() {
+        i++; console.log("clicked blue");
+        if ((questionArray[i].awnser) == "choiceB"){
+            correct++;
+            $("#totalCorrect").text(correct);
+    
+        }else{
+         incorrect++;
+        }
+         $("#totalWrong").text(incorrect)
+       loading();
+   })  
+   $("#green").on("click", function() {
+    i++; console.log("clicked green");
+    if ((questionArray[i].awnser) == $(choiceC)){
+        correct++;
+        $("#totalCorrect").text(correct);
+
+    }else{
+     incorrect++;
+    }
+     $("#totalWrong").text(incorrect)
+   loading();
+})
+$("#white").on("click", function() {
+    i++; console.log("clicked white");
+    if ((questionArray[i].awnser) == $(choiceD)){
+        correct++;
+        $("#totalCorrect").text(correct);
+
+    }else{
+     incorrect++;
+    }
+     $("#totalWrong").text(incorrect)
+   loading();
+})
 console.log(i)
 
-//listen for click event
-//
 
 //timer
 function start(){
@@ -121,7 +189,7 @@ function start(){
       }
   };
 
-//click funciton
+//click function
 //declare right and wrong questions
 
 });
